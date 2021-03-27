afb2a822fc2de2b95734ed96b5bb4c835884cedc
Dmitriy Kozlov <dimitry_kozlov@hotmail.com>
2021-03-27 21:05:10 +0100

    opt images

--- src/pages/Misomania.vue
+++ src/pages/Misomania.vue
@@ -12,14 +12,14 @@
 
       <div class="preview_container">
         <section v-for="image in bgImages" :key="image.id">
-          <div
+          <g-image
             to="/strasburg"
             :style="{
               'background-image':
                 'url(' + require('~/assets/' + image.src) + ')',
             }"
             class="preview_img"
-          ></div>
+          ></g-image>
           <g-link to="/strasburg" class="titleImage">
             <h2>{{ image.title }}</h2>
           </g-link>
